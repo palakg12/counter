417149163e56edf184f12d286f21d5cfd871f390
Palak <gpalak017@gmail.com>
2023-04-15 16:33:08 +0530

    .

---
+++ app.js
@@ -0,0 +1,22 @@
+let count=0;
+
+
+const value=document.querySelector("#value");
+const btns=document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click',function(e){
+        const styles=e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }
+        else if(styles.contains("reset")){
+            count=0;
+        }
+        else{
+            count++;
+        }
+        value.textContent=count
+    
+    })
+});
